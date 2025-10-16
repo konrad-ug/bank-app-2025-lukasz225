2d8c0b5cffc16a4addc31aa1aeb6d7d395f6d2a5
lukasz225 <lukaszszreder04@gmail.com>
2025-10-16 13:36:27 +0200

    lab1

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -2,7 +2,16 @@ from src.account import Account
 
 class TestAccount:
 	def test_account_creation(self):
-		account = Account("John", "Doe")
+		account = Account("John", "Doe", "02340174842")
 		assert account.first_name == "John"
 		assert account.last_name == "Doe"
 		assert account.balance == 0
+		assert account.pesel == '02340174842'
+
+	def test_pesel_too_short(self):
+		account = Account("Jane", "Doe", '123')
+		assert account.pesel == "Invalid"
+
+	def test_pesel_too_long(self):
+		account = Account("Jane", "Doe", '123123123123')
+		assert account.pesel == "Invalid"
