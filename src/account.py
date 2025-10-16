22459824ead5c7edf2b35efb9a86eb50def8206c
lukasz225 <lukaszszreder04@gmail.com>
2025-10-16 13:54:15 +0200

    lab1

--- src/account.py
+++ src/account.py
@@ -1,6 +1,11 @@
 class Account:
-	def __init__(self, first_name, last_name, pesel):
+	def __init__(self, first_name, last_name, pesel, promo_code = None):
 		self.first_name = first_name
 		self.last_name = last_name
 		self.balance = 0
-		self.pesel = pesel if len(pesel) == 11 else "Invalid"
+		self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+		if promo_code and promo_code.startswith("PROM_"):
+			self.balance += 50
+
+	def is_pesel_valid(self, pesel):
+		return isinstance(pesel, str) and len(pesel) == 11
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -15,3 +15,16 @@ class TestAccount:
 	def test_pesel_too_long(self):
 		account = Account("Jane", "Doe", '123123123123')
 		assert account.pesel == "Invalid"
+
+	def test_pesel_is_none(self):
+		account = Account("Jane", "Doe", None)
+		assert account.pesel == "Invalid"
+
+	def test_pesel_is_boolean(self):
+		account = Account("Jane", "Doe",  True)
+		assert account.pesel == "Invalid"
+
+
+	def test_account_with_valid_promo_code(self):
+		account = Account("Adam", "Nowak", "02340174842", promo_code="PROM_XYZ")
+		assert account.balance == 50
